c6d5a8ed8156c1ec6e754762c4f2ee4a88449128
Bór Milán <milanbor40@gmail.com>
2023-08-07 17:18:30 +0200

    feat(projects): projects page

--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -1,3 +1,53 @@
+import { Grid, Typography } from "@mui/material";
+import { textColor } from "../config";
+import { IProject } from "../types";
+import projectsJson from "../data/projects.json";
+import { getImageByName } from "../static";
+
 export default function Projects() {
-  return <>Projects</>;
+  const projects: IProject[] = projectsJson;
+
+  return (
+    <Grid
+      display="flex"
+      flexDirection="row"
+      justifyContent="space-around"
+      alignItems="center"
+      flexWrap="wrap"
+      sx={{ height: "100%", width: "100%" }}
+    >
+      {projects.map((project) => (
+        <Project project={project} />
+      ))}
+    </Grid>
+  );
+}
+
+type Props = {
+  project: IProject;
+};
+
+function Project({ project }: Props) {
+  return (
+    <Grid
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        height: "80%",
+        width: "40%",
+        minWidth: "400px",
+        borderRadius: "10px",
+        backgroundColor: textColor,
+      }}
+    >
+      <Typography variant="h4" sx={{ margin: "10px" }}>
+        {project.title}
+      </Typography>
+      <Typography variant="body1" sx={{ margin: "10px" }}>
+        {project.text}
+      </Typography>
+      {getImageByName(project.imgName)}
+    </Grid>
+  );
 }
--- client/src/types.ts
+++ client/src/types.ts
@@ -1,21 +1,38 @@
-export interface Page {
-  element: JSX.Element;
+export interface IPage {
   title: string;
+  component: JSX.Element;
 }
 
-export interface Problem {
+export interface IPageJson {
+  title: string;
+  componentName: string;
+}
+
+export interface IProblem {
   problem: string;
   solution: string;
   icon: string;
   tiltle: string;
 }
 
-export interface SolutionContent {
+export interface ISolutionContent {
   title: string;
   text: string;
 }
 
-export interface SolutionImage {
+export interface ISolutionImage {
   title: string;
   imageSrc: string;
+}
+
+export interface IProject {
+  title: string;
+  text: string;
+  imgName: string;
+}
+
+export interface ITeamMember {
+  name: string;
+  description: string;
+  image: string;
 }
